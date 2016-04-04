2bfeb20f113a63d4f7e1026f0c2c844ee570f484
Micaël <micael.mbagira@icloud.com>
2016-04-04 17:09:37 +0200

    Add travis-env

---
+++ test/travis-env.js
@@ -0,0 +1,36 @@
+'use strict';
+
+require('co-mocha');
+const product = require('cartesian-product');
+
+const gulp = require('./helpers/gulp-helper');
+const yeoman = require('./helpers/yeoman-helper');
+const wdio = require('./helpers/wdio-helper');
+
+describe('fountain travis integration test with saucelabs and webdriver.io', function () {
+  this.timeout(0);
+
+  before(function *() {
+    yield wdio.init();
+  });
+
+  const options = {
+    framework: process.env.FOUNTAIN_FRAMWORK,
+    modules: process.env.FOUNTAIN_MODULES,
+    css: 'scss',
+    js: process.env.FOUNTAIN_JS,
+    sample: 'techs'
+  };
+
+  it(`should work with ${options.framework}, ${options.modules}, ${options.js}`, function *() {
+    yield yeoman.prepare();
+    yield yeoman.run(options);
+    const url = yield gulp.serve();
+    yield wdio.techsTest(url);
+    gulp.killServe();
+  });
+  
+  after(function *() {
+    yield wdio.close();
+  });
+});
