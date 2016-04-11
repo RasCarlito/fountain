084b600d44399fd5b328eb3c60a49a05e9559a39
Micaël <micael.mbagira@icloud.com>
2016-04-11 15:49:39 +0200

    Add linter test to Travis-env

--- test/travis-env.js
+++ test/travis-env.js
@@ -5,6 +5,7 @@ require('co-mocha');
 const gulp = require('./helpers/gulp-helper');
 const yeoman = require('./helpers/yeoman-helper');
 const wdio = require('./helpers/wdio-helper');
+const linter = require('./helpers/linter-helper');
 
 describe('fountain travis integration test with saucelabs and webdriver.io', function () {
   this.timeout(0);
@@ -20,6 +21,10 @@ describe('fountain travis integration test with saucelabs and webdriver.io', fun
     js: process.env.FOUNTAIN_JS,
     sample: 'techs'
   };
+
+  it(`should test linter on ${options.framework}, ${options.modules}, ${options.js}`, function *() {
+    yield linter.linterTest(options);
+  });
 
   it(`should work with ${options.framework}, ${options.modules}, ${options.js}`, function *() {
     yield yeoman.prepare();
