e942c46543ce7c92e9634eed228101d470a72ee4
Swiip <matthieu.lux@gmail.com>
2016-07-17 22:43:33 +0200

    zip each folder

--- scripts/dist.js
+++ scripts/dist.js
@@ -2,58 +2,38 @@
 
 const path = require('path');
 const co = require('co');
-const product = require('cartesian-product');
 const helpers = require('yeoman-test');
 const mkdirp = require('mkdirp-promise');
 const rimraf = require('rimraf-promise');
 const Promise = require('bluebird');
 
 const output = require('./mute');
+const combinations = require('./helpers/combinations');
+const zip = require('./helpers/zip');
 
 const generatorPath = path.join(__dirname, '../generator-fountain-webapp/generators/app');
 
 try {
   co(function *() {
-    try {
-      yield rimraf(path.join(__dirname, `../dist`));
+    yield rimraf(path.join(__dirname, `../dist`));
 
-      const combinations = product([
-        ['react', 'angular1', 'angular2'],
-        ['webpack', 'systemjs', 'inject'],
-        ['babel', 'js', 'typescript'],
-        ['css', 'scss', 'less', 'styl'],
-        ['hello', 'techs', 'todoMVC']
-      ])
-        // Angular 2 and Bower are not supported right now
-        .filter(combination => combination[0] !== 'angular2' || combination[1] !== 'inject');
-
-      for (const combination of combinations) {
-        const options = {
-          framework: combination[0],
-          modules: combination[1],
-          css: combination[3],
-          js: combination[2],
-          sample: combination[4],
-          router: combination[0] === 'angular1' ? 'uirouter' : 'router'
-        };
-        const combinationPath = path.join(__dirname, `../dist/${options.framework}-${options.modules}-${options.js}-${options.css}-${options.router}-${options.sample}`);
-        yield mkdirp(combinationPath);
-        const fountain = helpers.createGenerator('fountain-webapp:app', [generatorPath], null, {
-          'skipInstall': true,
-          'skip-welcome-message': true
-        });
-        fountain.env.cwd = combinationPath;
-        helpers.mockPrompt(fountain, options);
-        const run = Promise.promisify(fountain.run.bind(fountain));
-        output.mute();
-        yield run();
-        output.unmute();
-        console.log('Generated', combinationPath);
-      }
-    } catch (error) {
-      console.log('coucou 2', error);
+    for (const options of combinations.full()) {
+      const combinationPath = path.join(__dirname, `../dist/${options.framework}-${options.modules}-${options.js}-${options.css}-${options.router}-${options.sample}`);
+      yield mkdirp(combinationPath);
+      const fountain = helpers.createGenerator('fountain-webapp:app', [generatorPath], null, {
+        'skipInstall': true,
+        'skip-welcome-message': true
+      });
+      fountain.env.cwd = combinationPath;
+      helpers.mockPrompt(fountain, options);
+      const run = Promise.promisify(fountain.run.bind(fountain));
+      output.mute();
+      yield run();
+      output.unmute();
+      yield zip.zipFolder(combinationPath, `${combinationPath}.zip`);
+      console.log('Generated', combinationPath);
     }
   });
 } catch (error) {
-  console.log('coucou', error);
+  console.log('Something went wrong', error);
 }
