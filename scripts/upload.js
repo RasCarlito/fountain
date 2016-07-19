b819716f227fa50ce4cfb44efc581305f4dace1a
Swiip <matthieu.lux@gmail.com>
2016-07-19 14:45:59 +0200

    first version of github upload script

--- scripts/upload.js
+++ scripts/upload.js
@@ -1,3 +1,37 @@
-console.log('upload script');
+const fs = require('fs');
+const path = require('path');
+const co = require('co');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
-console.log('env GIT_NAME', process.env.GIT_NAME);
+const combinations = require('./helpers/combinations');
+
+try {
+  co(function *() {
+    for (const options of combinations.full()) {
+      const fileName = `${options.framework}-${options.modules}-${options.js}-${options.css}-${options.router}-${options.sample}.zip`;
+      const combinationPath = path.join(__dirname, `../dist/${fileName}`);
+
+      const form = new FormData();
+      form.append('name', fileName);
+      form.append(fileName, fs.createReadStream(combinationPath), {
+        headers: {
+          'Authorization': process.env.GITHUB_TOKEN,
+          'Content-Type': 'application/zip'
+        }
+      });
+
+      const url = `https://github.com/repos/FountainJS/fountain/releases/${process.env.TRAVIS_TAG}/assets`;
+
+      console.log('Uploading', fileName, 'to', url);
+
+      yield fetch(url, {
+        method: 'POST',
+        body: form,
+        headers: form.getHeaders()
+      });
+    }
+  });
+} catch (error) {
+  console.log('Something went wrong', error);
+}
