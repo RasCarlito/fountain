7db1945e20243d54265f8630b489815e9d1ce69b
Swiip <matthieu.lux@gmail.com>
2016-07-20 10:57:08 +0200

    new upload script

--- scripts/upload.js
+++ scripts/upload.js
@@ -2,34 +2,67 @@ const fs = require('fs');
 const path = require('path');
 const co = require('co');
 const fetch = require('node-fetch');
-const FormData = require('form-data');
 
 const combinations = require('./helpers/combinations');
 
+const githubApiUrl = 'https://api.github.com/repos/FountainJS/fountain';
+const githubUploadUrl = 'https://uploads.github.com/repos/FountainJS/fountain';
+
+function githubRequest(rootUrl, partialUrl, params) {
+  if (!params.headers) {
+    params.headers = {};
+  }
+  params.headers.Authorization = `token ${process.env.GITHUB_TOKEN}`;
+
+  console.log(params.method, partialUrl);
+
+  return fetch(`${rootUrl}${partialUrl}`, params)
+    .then(response => {
+      if (response.status === 200) {
+        return response.json();
+      }
+      return response.text();
+    })
+    .then(response => {
+      console.log(response);
+      return response;
+    })
+    .catch(console.error);
+}
+
+function githubApiRequest(partialUrl, params) {
+  return githubRequest(githubApiUrl, partialUrl, params);
+}
+
+function githubUploadRequest(partialUrl, params) {
+  return githubRequest(githubUploadUrl, partialUrl, params);
+}
+
 try {
   co(function *() {
+    yield githubApiRequest('/releases', {
+      method: 'POST',
+      body: JSON.stringify({tag_name: process.env.TRAVIS_TAG}) // eslint-disable-line camelcase
+    });
+
+    const tag = yield githubApiRequest(`/releases/tags/${process.env.TRAVIS_TAG}`, {
+      method: 'GET'
+    });
+
     for (const options of combinations.full()) {
       const fileName = `${options.framework}-${options.modules}-${options.js}-${options.css}-${options.router}-${options.sample}.zip`;
       const combinationPath = path.join(__dirname, `../dist/${fileName}`);
 
-      const form = new FormData();
-      form.append('name', fileName);
-      form.append(fileName, fs.createReadStream(combinationPath), {
+      const body = fs.readFileSync(combinationPath);
+
+      yield githubUploadRequest(`/releases/${tag.id}/assets?name=${fileName}`, {
+        method: 'POST',
+        body,
         headers: {
-          'Authorization': process.env.GITHUB_TOKEN,
-          'Content-Type': 'application/zip'
+          'Content-Type': 'application/zip',
+          'Content-Length': body.length
         }
       });
-
-      const url = `https://github.com/repos/FountainJS/fountain/releases/${process.env.TRAVIS_TAG}/assets`;
-
-      console.log('Uploading', fileName, 'to', url);
-
-      yield fetch(url, {
-        method: 'POST',
-        body: form,
-        headers: form.getHeaders()
-      }).catch(console.error);
     }
   });
 } catch (error) {
