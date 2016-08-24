df1b6cae9f33fb80e02b7b0feafd8c282cb87df2
Matthieu Lux <matthieu.lux@gmail.com>
2016-08-24 17:56:54 +0200

    Publish binaries of all combinations (#43)
    * add script generating binaries for all combinations
    
    * zip each folder
    
    * test configuration for travis and upload script
    
    * first version of github upload script
    
    * deploy bin with node
    
    * correct boolean test
    
    * try to detect dist bug
    
    * move deploy in script to be before the cache cleanup
    
    * new upload script
    
    * new token
    
    * add strict mode for node 4
    
    * switch to spawned curl commands
    
    * show api responses
    
    * fix json body arg
    
    * fix curl file upload
    
    * add tag on meta repo when publishing to trigger deployment
    
    * free up integration test before deployment
    
    * log github upload responses
    
    * try a sleep between uploads as a workaround of github errors
    
    * unmute yeoman to understand why dist fails
    
    * bump submodules and rollback mute yeoman
    
    * bump submodules
    
    * bump submodules (again)
    
    * add ci option in publish combinations
    
    * Indent travis.yml, move archiver dep into devDep

---
+++ scripts/dist.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+const co = require('co');
+const helpers = require('yeoman-test');
+const mkdirp = require('mkdirp-promise');
+const rimraf = require('rimraf-promise');
+const Promise = require('bluebird');
+
+const output = require('./helpers/mute');
+const combinations = require('./helpers/combinations');
+const zip = require('./helpers/zip');
+
+const generatorPath = path.join(__dirname, '../generator-fountain-webapp/generators/app');
+
+co(function *() {
+  try {
+    yield rimraf(path.join(__dirname, `../dist`));
+
+    for (const options of combinations.full()) {
+      const combinationPath = path.join(__dirname, `../dist/${options.framework}-${options.modules}-${options.js}-${options.css}-${options.router}-${options.sample}`);
+      yield mkdirp(combinationPath);
+      const fountain = helpers.createGenerator('fountain-webapp:app', [generatorPath], null, {
+        'skipInstall': true,
+        'skip-welcome-message': true
+      });
+      fountain.env.cwd = combinationPath;
+      helpers.mockPrompt(fountain, options);
+      const run = Promise.promisify(fountain.run.bind(fountain));
+      output.mute();
+      yield run();
+      output.unmute();
+      yield zip.zipFolder(combinationPath, `${combinationPath}.zip`);
+      console.log('Generated', combinationPath);
+    }
+  } catch (error) {
+    console.log('Something went wrong', error);
+  }
+});
---
+++ scripts/helpers/combinations.js
@@ -0,0 +1,24 @@
+const product = require('cartesian-product');
+
+const frameworks = ['react', 'angular1', 'angular2'];
+const modules = ['webpack', 'systemjs', 'inject'];
+const js = ['babel', 'js', 'typescript'];
+const css = ['css', 'scss', 'less', 'styl'];
+const sample = ['hello', 'techs', 'todoMVC'];
+
+// Angular 2 and Bower are not supported right now
+const ng2bowerFilter = combination => combination[0] !== 'angular2' || combination[1] !== 'inject';
+
+exports.full = function full() {
+  return product([frameworks, modules, js, css, sample])
+    .filter(ng2bowerFilter)
+    .map(combination => ({
+      framework: combination[0],
+      modules: combination[1],
+      js: combination[2],
+      css: combination[3],
+      sample: combination[4],
+      router: combination[0] === 'angular1' ? 'uirouter' : 'router',
+      ci: []
+    }));
+};
